57df6a2d170fe73e024512b61559f0f14d284064
nina <juan.amado.sicat.cleto@gmail.com>
2025-06-27 22:10:58 +0800

    First Commit

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+//Program that takes individual words
+//Would count number of words in a file
+//And then once that is done it would count the number of letters within each word
+//would count the number of numbers in that file
+
+void runProgram() {
+    printf("========================================\n");
+    printf("<1> \n");
+}
+
+
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
 }
