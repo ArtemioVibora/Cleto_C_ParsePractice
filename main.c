b04c3dc32257c8c0f5f76d9ff4d882c29ec9e172
nina <juan.amado.sicat.cleto@gmail.com>
2025-06-27 22:38:05 +0800

    First Commit

--- main.c
+++ main.c
@@ -4,13 +4,33 @@
 //Would count number of words in a file
 //And then once that is done it would count the number of letters within each word
 //would count the number of numbers in that file
+//would count the number of special characters
+
+//Test
+int countWords(char string[]) {
+    int numWords = 1;
+
+    if (string[0] == '\0') {
+        numWords = 0;
+    }
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] == ' ' && string[i + 1] != ' ') {
+            numWords++;
+        }
+    }
+
+    return numWords;
+}
 
 void runProgram() {
-    printf("========================================\n");
-    printf("<1> \n");
+    char string[] = "Hello World! Friend";
+    char string2[] = "";
+    printf("the number of words is %d\n", countWords(string));
+    printf("the number of words is %d\n", countWords(string2));
 }
 
 
 int main(void) {
     runProgram();
 }
+
